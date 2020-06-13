6c510a36797ddb90cad3d014d3f2a09830457cc9
grdm13 <grdmgrdm@gmail.com>
2020-06-13 11:32:56 -0500

    works for the binary tamal images
    returns numpy array with 5 stats

--- 1_scripts/finds_same_droplet_creates_lines.py
+++ 1_scripts/finds_same_droplet_creates_lines.py
@@ -13,6 +13,8 @@ from_57 = np.array(
      [243, 365, 1, 1, 119]), float)
 
 
+
+
 def check_for_HOR_VER_matchups(array1, array2):
     matching_HOR_VER = []
     Centroids_match = []
@@ -38,8 +40,8 @@ def create_3_lines(list):
     matrix = np.zeros((1000,3), float)
     for x in range(1000):
         y1 = a*x + b
-        y2 = (a-0.5)*x + b
-        y3 = (a+0.5) *x + b
+        y2 = y1 + b
+        y3 = y1 - b
         matrix[x][0] = y1
         matrix[x][1] = y2
         matrix[x][2] = y3
@@ -48,7 +50,39 @@ def create_3_lines(list):
     plt.plot(matrix)
     plt.plot(list[0], list[1], color='green', marker='o')
     plt.plot(list[2], list[3], color='red', marker='o')
+    plt.plot(10, 24, color='yellow', marker='o') #this is only for this test
+    plt.xlim(xmin=0, xmax=413)
+    plt.ylim(ymin=0, ymax = 487)
+    ax = plt.gca()  # get the axis
+    ax.set_ylim(ax.get_ylim()[::-1])  # invert the axis
+    ax.xaxis.tick_top()  # and move the X-Axis
     plt.show()
 
+
+
+
+
+
+
+
+
+
+
+
 create_3_lines(check_for_HOR_VER_matchups(from_56, from_57)[0])
-create_3_lines(check_for_HOR_VER_matchups(from_56, from_57)[1])
+
+
+#from 55
+#[[ 99  70  13  17 127] <------
+# [233 165  12  14 119]]
+
+#from 56
+#[[ 56  47  13  17 140] <------
+ #[123 219  16  16 185]
+ #[ 88 132  16  16 140]]
+
+ #from 57
+#[[ 10  24  20  16 176]<----- it must find this guy here
+# [ 77 199  33  19  52]
+# [129  35  25  13 142]
+# [119 109  16  15 150]]
